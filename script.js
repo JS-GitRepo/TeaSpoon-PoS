51e3430fbc86faaa684d264892be1e2643f524a1
ncrown1190 <ncrown88@outlook.com>
2022-02-17 13:09:26 -0500

    array in script

---
+++ script.js
@@ -0,0 +1,62 @@
+const products = [
+  {
+    name: "green",
+    category: "tea",
+    description: "freshly prepared",
+    price: 3.5,
+  },
+  {
+    name: "black",
+    category: "tea",
+    description: "freshly prepared",
+    price: 3.5,
+  },
+  {
+    name: "oolong",
+    category: "tea",
+    description: "freshly prepared",
+    price: 4.5,
+  },
+  {
+    name: "herbal",
+    category: "tea",
+    description: "freshly prepared",
+    price: 4.25,
+  },
+  {
+    name: "flower",
+    category: "tea",
+    description: "freshly prepared",
+    price: 4.25,
+  },
+  {
+    name: "chai",
+    category: "tea",
+    description: "freshly prepared",
+    price: 4.25,
+  },
+  {
+    name: "matcha",
+    category: "tea",
+    description: "freshly prepared",
+    price: 4.25,
+  },
+  {
+    name: "coffee",
+    category: "coffee",
+    description: "freshly prepared",
+    price: 4.25,
+  },
+  {
+    name: "Latte",
+    category: "coffee",
+    description: "freshly prepared",
+    price: 4.25,
+  },
+  {
+    name: "chocolate cookie",
+    category: "snack",
+    description: "freshly prepared",
+    price: 4.25,
+  },
+];
